21b5c6c2f76781472e3f5947057d031ac81456fb
Daiana <limabrito.dai@gmail.com>
2026-01-21 13:20:33 -0800

    Updating permissions

--- common.sh
+++ common.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 echo "[+] Updating system"
-apt update -y
-apt upgrade -y
+sudo apt update -y
+sudo apt upgrade -y
 echo "[+] Installing common packages"
-apt install -y curl git ufw net-tools dnsutils python3 python3-pip
+sudo apt install -y curl git ufw net-tools dnsutils python3 python3-pip
